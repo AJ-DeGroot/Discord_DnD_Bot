d34d4cb5b56c47888e7083bacb0e4fa82e1f42d9
AJ-DeGroot <alan.michael.degroot@gmail.com>
2018-06-09 18:20:05 -0400

    Outputs random rolls (#d#+#), +# is optional.

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 
+const times = x => f => {
+  if (x > 0) {
+    f()
+    times (x - 1) (f)
+  }
+}
+
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online.`);
   bot.user.setActivity('!roll', {type: 'STREAMING' })
@@ -18,10 +25,36 @@ bot.on("message", async message => {
 
   if(cmd === `${prefix}roll`){
     let newArgs = args;
-    newArgs.split('')
-    console.log(newArgs);
-    let result = Math.floor(Math.random() * 10);
-    return message.channel.send(`${result}`);
+    let diceResults = [];
+    let diceTotal = [];
+    let rollBonus = 0;
+
+    for (var x of newArgs){
+      console.log(newArgs);
+      let diceCount = x[0];
+      console.log(diceCount);
+      if (isNaN(x.split("+")[1])){
+
+      } else {
+      rollBonus = Number(rollBonus) + Number(x.split("+")[1]);
+      console.log(rollBonus);
+      }
+      let diceSize = x.split("+")[0].slice(2);
+      console.log(diceSize);
+
+      times (diceCount) (() => diceResults.push((Math.floor(Math.random() * diceSize) + 1)));
+    }
+
+    for (var n of diceResults){
+      diceTotal = Number(diceTotal) + Number(n);
+    }
+
+    diceTotal = Number(diceTotal) + Number(rollBonus);
+
+    console.log(diceResults);
+    message.delete().catch(O_o=>{});
+    return message.channel.send(`${message.author} rolled: **${diceTotal}**.\n ${diceResults}.\n ${args}`);
+
   }
 });
 
